07576ce2f71f69070a9d8e829ca5b7121a7219a4
Pedro Paulo Cunha <pedropaulocunha.dev@gmail.com>
2024-07-27 20:33:42 -0300

    added edit function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { listReducer } from "@/reducers/listReducer";
 import { useReducer, useState } from "react";
+import { text } from "stream/consumers";
 
 const Page =()=> {
   const [list, dispatch] = useReducer(listReducer, []);
@@ -26,7 +27,23 @@ const Page =()=> {
 
   }
 
+  const handleEdit = (id:number) => {
+    const item = list.find(it => it.id ===id);
+    if (!item) return false;
+
+    const newText = window.prompt("Editar tarefa", item.text)
+    if(!newText || newText?.trim() === '') return false;
+    dispatch({
+      type:"editText",
+      payload: {
+        id,
+        newText: newText
+      }
+    })    
+  }
+
   const handleRemoveClick = (id: number)=> {
+    if(!window.confirm("excluir item?")) return false;
     dispatch({
       type: 'remove',
       payload: {id}
@@ -38,13 +55,15 @@ const Page =()=> {
       <h1 className="text-center text-4xl my-4">Lista de tarefas</h1>
       <div className="max-w-2xl mx-auto flex rounded-md border border-gray-400 p-4 my-4">
         <input
-        type="text"
-        className="flex-1 rounded-md border border-black p-3 bg-transparent outline-none"
-        placeholder="Digite um item"
-        value={addField}
-        onChange={e=> setAddField(e.target.value)}
+          type="text"
+          className="flex-1 rounded-md border border-black p-3 bg-transparent outline-none"
+          placeholder="Digite um item"
+          value={addField}
+          onChange={e=> setAddField(e.target.value)}
         />
-        <button className="p-4"onClick={handleAddButton}>ADD</button>
+        <button 
+          className="p-4"
+          onClick={handleAddButton}>ADD</button>
       </div>
       <ul className="max-w-2xl mx-auto">
         {list.map(item=>(
@@ -60,15 +79,12 @@ const Page =()=> {
             <p className="flex-1 text-lg">{item.text}</p>
             <button 
               className="mx-4 hover:text-blue-300"
-            >
-              Edit
-            </button>
+              onClick={()=> handleEdit(item.id)}
+            >Edit</button>
             <button 
               className="mx-4 hover:text-blue-300"
               onClick={()=>handleRemoveClick(item.id)}
-            >
-              excluir
-            </button>
+            >excluir</button>
           </li>
         ))}
       </ul>
